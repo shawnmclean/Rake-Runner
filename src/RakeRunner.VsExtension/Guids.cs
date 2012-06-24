6bb032c35f8009305d0eb54143df180c822ee2f9
Shawn Mclean <mixmasterxp@gmail.com>
2012-06-24 01:37:16 -0500

    Added a vs extension package

---
+++ src/RakeRunner.VsExtension/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.RakeRunner_VsExtension
+{
+    static class GuidList
+    {
+        public const string guidRakeRunnerPkgString = "250f7ad3-190d-489e-b8f9-9a0604c2ac72";
+        public const string guidRakeRunnerCmdSetString = "c78d067c-81eb-42ee-b3c6-5ad5ebec72ae";
+
+        public static readonly Guid guidRakeRunnerCmdSet = new Guid(guidRakeRunnerCmdSetString);
+    };
+}
---
+++ src/RakeRunner.VsExtension/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Company.RakeRunner_VsExtension
+{
+    static class PkgCmdIDList
+    {
+        public const uint runRakeTaskCommandId =        0x100;
+
+
+    };
+}
