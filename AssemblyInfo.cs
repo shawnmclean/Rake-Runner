6a512d68bbd197bd4c35744ee87f305f05a44bdc
Shawn Mclean <luckyparrot@hotmail.com>
2012-07-15 15:16:41 -0500

    Bump version to 0.0.0.2, does not hang on build.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Self")]
 [assembly: AssemblyProduct("Rake Runner")]
 [assembly: AssemblyCopyright("Shawn Mclean (c) 2012")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.0.0.2")]
+[assembly: AssemblyFileVersion("0.0.0.2")]
 
