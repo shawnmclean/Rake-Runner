59973ff0ff830c172536b2826dc7a4e4ed570fae
Shawn Mclean <luckyparrot@hotmail.com>
2012-07-15 10:41:07 -0500

    Rake now outputs to build window

--- src/RakeRunner.VsExtension/RakeRunner.cs
+++ src/RakeRunner.VsExtension/RakeRunner.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.ComponentModel.Design;
-using System.Windows.Forms;
 using EnvDTE;
-using Microsoft.Win32;
 using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
-using RakeRunner.Library.Exceptions;
+using Microsoft.VisualStudio.Shell.Interop;
 using RakeRunner.Library.Models;
 using RakeRunner.Library.Services;
 
@@ -24,8 +20,8 @@ namespace RakeRunner
     /// The minimum requirement for a class to be considered a valid package for Visual Studio
     /// is to implement the IVsPackage interface and register itself with the shell.
     /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the 
-    /// IVsPackage interface and uses the registration attributes defined in the framework to 
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
     /// register itself and its components with the shell.
     /// </summary>
     // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
@@ -40,10 +36,9 @@ namespace RakeRunner
     [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string)]
     //[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string)]
     [Guid(GuidList.guidRakeRunnerPkgString)]
-    public class RakeRunner : 
+    public class RakeRunner :
         Package,
         IVsSolutionEvents3,
-        IVsFileChangeEvents,
         IVsSelectionEvents,
         IDisposable
     {
@@ -53,9 +48,9 @@ namespace RakeRunner
 
         /// <summary>
         /// Default constructor of the package.
-        /// Inside this method you can place any initialization code that does not require 
-        /// any Visual Studio service because at this point the package object is created but 
-        /// not sited yet inside Visual Studio environment. The place to do all the other 
+        /// Inside this method you can place any initialization code that does not require
+        /// any Visual Studio service because at this point the package object is created but
+        /// not sited yet inside Visual Studio environment. The place to do all the other
         /// initialization is the Initialize method.
         /// </summary>
         public RakeRunner()
@@ -63,10 +58,9 @@ namespace RakeRunner
             Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
         }
 
-
-
         /////////////////////////////////////////////////////////////////////////////
         // Overridden Package Implementation
+
         #region Package Members
 
         /// <summary>
@@ -75,12 +69,11 @@ namespace RakeRunner
         /// </summary>
         protected override void Initialize()
         {
-            Debug.WriteLine (string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
             base.Initialize();
 
-
             rakeService = new RakeService();
-            
+
             this._dte = (DTE)this.GetService(typeof(DTE));
 
             IVsMonitorSelection ms = this.GetService(typeof(SVsShellMonitorSelection)) as IVsMonitorSelection;
@@ -91,28 +84,23 @@ namespace RakeRunner
             if (solution != null)
                 solution.AdviseSolutionEvents(this, out _vsSolutionEventsCookie);
 
-
             // Add our command handlers for menu (commands must exist in the .vsct file)
             OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if ( null != mcs )
+            if (null != mcs)
             {
                 //the main menu
                 CommandID command = new CommandID(GuidList.guidRakeRunnerCmdSet, (int)PkgCmdIDList.icmdCommandRakeMenu);
                 var menu = new OleMenuCommand(null, command);
                 mcs.AddCommand(menu);
-                
             }
         }
 
-        #endregion
-
-
+        #endregion Package Members
 
         #region Implementation of IVsSolutionEvents
 
         public int OnAfterOpenSolution([InAttribute] Object pUnkReserved, [InAttribute] int fNewSolution)
         {
-            monitorFileChange();
             return VSConstants.S_OK;
         }
 
@@ -128,8 +116,6 @@ namespace RakeRunner
 
         public int OnAfterOpenProject([InAttribute] IVsHierarchy pHierarchy, [InAttribute] int fAdded)
         {
-
-            
             return VSConstants.S_OK;
         }
 
@@ -216,7 +202,7 @@ namespace RakeRunner
             return VSConstants.S_OK;
         }
 
-        #endregion
+        #endregion Implementation of IVsSolutionEvents
 
         #region Implementation of IDisposable
 
@@ -226,8 +212,6 @@ namespace RakeRunner
         /// <filterpriority>2</filterpriority>
         public void Dispose()
         {
-            unmonitorFileChange();
-
             // Unregister from receiving solution events
             if (VSConstants.VSCOOKIE_NIL != _vsSolutionEventsCookie)
             {
@@ -240,64 +224,9 @@ namespace RakeRunner
             }
         }
 
-        #endregion
+        #endregion Implementation of IDisposable
 
         #region Files and Directories
-
-        /// <summary>
-        /// Notifies clients of changes made to one or more files.
-        /// </summary>
-        /// <returns>
-        /// If the method succeeds, it returns <see cref="F:Microsoft.VisualStudio.VSConstants.S_OK"/>. If it fails, it returns an error code.
-        /// </returns>
-        /// <param name="cChanges">[in] Number of files changed.</param><param name="rgpszFile">[in, size_is(cChanges)] Array of file names.</param><param name="rggrfChange">[in, size_is(cChanges)] Array of flags indicating the type of changes. See <see cref="T:Microsoft.VisualStudio.Shell.Interop._VSFILECHANGEFLAGS"/>.</param>
-        public int FilesChanged(uint cChanges, string[] rgpszFile, uint[] rggrfChange)
-        {
-            return VSConstants.S_OK;
-        }
-
-        /// <summary>
-        /// Notifies clients of changes made to a directory.
-        /// </summary>
-        /// <returns>
-        /// If the method succeeds, it returns <see cref="F:Microsoft.VisualStudio.VSConstants.S_OK"/>. If it fails, it returns an error code.
-        /// </returns>
-        /// <param name="pszDirectory">[in] Name of the directory that had a change.</param>
-        public int DirectoryChanged(string pszDirectory)
-        {
-            return VSConstants.S_OK;
-        }
-
-
-        private void monitorFileChange()
-        {
-            var solutionFileName = getSolutionFileName();
-
-            if (!string.IsNullOrEmpty(solutionFileName))
-            {
-                var monitorFolder = Path.GetDirectoryName(solutionFileName);
-
-                IVsFileChangeEx fileChangeService = this.GetService(typeof(SVsFileChangeEx)) as IVsFileChangeEx;
-                if (VSConstants.VSCOOKIE_NIL != _vsIVsFileChangeEventsCookie)
-                {
-                    fileChangeService.UnadviseFileChange(_vsIVsFileChangeEventsCookie);
-                }
-                fileChangeService.AdviseFileChange(monitorFolder, 1, this, out _vsIVsFileChangeEventsCookie);
-                Debug.WriteLine("==== Monitoring: " + monitorFolder + " " + _vsIVsFileChangeEventsCookie);
-
-            }
-        }
-
-        private void unmonitorFileChange()
-        {
-            if (VSConstants.VSCOOKIE_NIL != _vsIVsFileChangeEventsCookie)
-            {
-                IVsFileChangeEx fileChangeService = this.GetService(typeof(SVsFileChangeEx)) as IVsFileChangeEx;
-                fileChangeService.UnadviseDirChange(_vsIVsFileChangeEventsCookie);
-                Debug.WriteLine("==== Stop Monitoring: " + _vsIVsFileChangeEventsCookie.ToString());
-                _vsIVsFileChangeEventsCookie = VSConstants.VSCOOKIE_NIL;
-            }
-        }
 
         /// <summary>
         /// Returns the filename of the solution
@@ -305,7 +234,7 @@ namespace RakeRunner
         private string getSolutionFileName()
         {
             IVsSolution sol = (IVsSolution)this.GetService(typeof(SVsSolution));
-            
+
             string solutionDirectory, solutionFile, solutionUserOptions;
             if (sol.GetSolutionInfo(out solutionDirectory, out solutionFile, out solutionUserOptions) == VSConstants.S_OK)
             {
@@ -316,8 +245,8 @@ namespace RakeRunner
                 return null;
             }
         }
-        #endregion
 
+        #endregion Files and Directories
 
         #region Implementation of IVsSelectionEvents
 
@@ -331,8 +260,8 @@ namespace RakeRunner
         public int OnSelectionChanged(IVsHierarchy pHierOld, uint itemidOld, IVsMultiItemSelect pMISOld, ISelectionContainer pSCOld, IVsHierarchy pHierNew, uint itemidNew, IVsMultiItemSelect pMISNew, ISelectionContainer pSCNew)
         {
             string fullPath = "";
-            if(pHierNew != null)
-            {   
+            if (pHierNew != null)
+            {
                 // Get the full path
                 pHierNew.GetCanonicalName(itemidNew, out fullPath);
             }
@@ -346,7 +275,7 @@ namespace RakeRunner
             currentDir = directory;
             // Setup the rake menu for this path
             setupRakeTasksMenu(directory);
-            return VSConstants.S_OK; 
+            return VSConstants.S_OK;
         }
 
         /// <summary>
@@ -373,11 +302,11 @@ namespace RakeRunner
             return VSConstants.S_OK;
         }
 
-        #endregion
+        #endregion Implementation of IVsSelectionEvents
 
         #region Custom Code
 
-        private Dictionary<string, List<RakeTask>>  taskCache = new Dictionary<string, List<RakeTask>>();
+        private Dictionary<string, List<RakeTask>> taskCache = new Dictionary<string, List<RakeTask>>();
         private List<int> currentCommandsInMenu = new List<int>();
         private string currentDir = "";
 
@@ -400,9 +329,7 @@ namespace RakeRunner
             }
             catch
             {
-                
             }
-
         }
 
         private void setupRakeTasksMenu(string dir)
@@ -411,7 +338,7 @@ namespace RakeRunner
             {
                 List<RakeTask> tasks;
                 //update if cache not exist
-                if(!taskCache.ContainsKey(dir))
+                if (!taskCache.ContainsKey(dir))
                 {
                     updateCache(dir);
                 }
@@ -419,7 +346,6 @@ namespace RakeRunner
                 taskCache.TryGetValue(dir, out tasks);
 
                 OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-               
 
                 // Add our command handlers for menu (commands must exist in the .vsct file)
                 if (null != mcs)
@@ -432,7 +358,6 @@ namespace RakeRunner
                         var menu = mcs.FindCommand(cmdID);
                         if (menu != null)
                             mcs.RemoveCommand(menu);
-                        
                     }
                     //clear the commands list
                     currentCommandsInMenu.Clear();
@@ -442,7 +367,7 @@ namespace RakeRunner
                         //add the tasks
                         for (int i = 0; i < tasks.Count; i++)
                         {
-                            int commandId = (int) PkgCmdIDList.icmdTasksList + i;
+                            int commandId = (int)PkgCmdIDList.icmdTasksList + i;
                             var cmdID = new CommandID(
                                 GuidList.guidRakeRunnerCmdSet, commandId);
                             var mc = new OleMenuCommand(RakeTaskSelected, cmdID);
@@ -457,7 +382,6 @@ namespace RakeRunner
             }
             catch
             {
-                
             }
         }
 
@@ -471,10 +395,6 @@ namespace RakeRunner
                 if (taskIndex >= 0 && taskIndex < taskCache[currentDir].Count)
                 {
                     var selection = taskCache[currentDir][taskIndex];
-                    
-                    System.Windows.Forms.MessageBox.Show(
-                        string.Format(CultureInfo.CurrentCulture,
-                                      "Selected {0}", selection.Task));
                     runRakeTask(selection, currentDir);
                 }
             }
@@ -482,32 +402,33 @@ namespace RakeRunner
 
         private void runRakeTask(RakeTask task, string dir)
         {
-            rakeService.RunRakeTask(dir, task.Task);
+            clearOutputWindow();
+            rakeService.RunRakeTask(dir, task.Task, writeOutputWindow, writeOutputWindow);
         }
 
-        private void WriteOutputWindow(string outputText)
+        private void writeOutputWindow(string outputText)
         {
-            IVsOutputWindow outputWindow = Package.GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            var windowPane = getOutputWindow();
+            windowPane.Activate();
+            windowPane.OutputString(outputText);
+        }
 
-            // If we fail to get it we can exit now. 
-            if (null == outputWindow)
-            {
-                Trace.WriteLine("Failed to get a reference to IVsOutputWindow");
-                return;
-            }
-            // Now get the window pane for the general output. 
-            Guid guidGeneral = Microsoft.VisualStudio.VSConstants.GUID_OutWindowGeneralPane;
-            IVsOutputWindowPane windowPane = null;
-            if (Microsoft.VisualStudio.ErrorHandler.Failed(outputWindow.GetPane(ref guidGeneral, out windowPane)))
-            {
-                Trace.WriteLine("Failed to get a reference to the Output window General pane");
-                return;
-            }
-            if (Microsoft.VisualStudio.ErrorHandler.Failed(windowPane.OutputString(outputText)))
-            {
-                Trace.WriteLine("Failed to write on the Output window");
-            }
-        } 
-        #endregion
+        private void clearOutputWindow()
+        {
+            var windowPane = getOutputWindow();
+            windowPane.Clear();
+        }
+
+        private IVsOutputWindowPane getOutputWindow()
+        {
+            IVsOutputWindow outWindow = GetService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+            if (outWindow == null) throw new ArgumentNullException("outWindow");
+            Guid generalWindowGuid = VSConstants.GUID_BuildOutputWindowPane;
+            IVsOutputWindowPane windowPane;
+            outWindow.GetPane(ref generalWindowGuid, out windowPane);
+            return windowPane;
+        }
+
+        #endregion Custom Code
     }
 }
